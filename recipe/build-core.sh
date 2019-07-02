24962089817db4d0681aa58eb89cd278be0778e8
Marius van Niekerk <marius.v.niekerk@gmail.com>
2019-07-01 23:06:31 -0400

    Update build-core.sh

--- recipe/build-core.sh
+++ recipe/build-core.sh
@@ -37,12 +37,12 @@ if [[ $(uname) == 'Darwin' ]]; then
 elif [[ $(uname) == 'Linux' ]]; then
   # testsanitizers hangs > 10minutes
   if [[ ${cgo_var} == 'cgo' ]]; then
-    export GO_LFFLAGS=${LGFLAGS}
+    export GO_LFFLAGS=${LDFLAGS}
     export GO_EXTLINK_ENABLED=1
     # TODO: For future versions of go
     # export GO_LDSO= ld from conda-forge
   fi
-  ./make.bash
+  ./make.bash -v
 fi
 popd
 
