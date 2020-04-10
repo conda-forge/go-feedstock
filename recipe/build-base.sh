4ccc4b0a3c83b7d6ec153e22217e7bb0267381c9
Patrick Sodré <psodre@gmail.com>
2020-04-10 18:18:45 -0400

    !fixup

--- recipe/build-base.sh
+++ recipe/build-base.sh
@@ -1,36 +1,5 @@
 set -euf
 
-# Enable CGO, and set compiler flags to match conda-forge settings
-export CGO_ENABLED=1
-export CGO_CFLAGS=${CFLAGS}
-export CGO_CPPFLAGS=${CPPFLAGS}
-export CGO_CXXFLAGS=${CXXFLAGS}
-export CGO_LDFLAGS=${LDFLAGS}
-case $(uname -s) in
-  Darwin)
-    # Tell it where to find the MacOS SDK
-    export CGO_CPPFLAGS="${CGO_CPPFLAGS} -isysroot ${CONDA_BUILD_SYSROOT}"
-    ;;
-  Linux)
-    # We have to disable garbage collection for sections
-    export CGO_LDFLAGS="${CGO_LDFLAGS} -Wl,--no-gc-sections"
-    ;;
-  *)
-    echo "Unknown OS: $(uname -s)"
-    exit 1
-    ;;
-esac
-
-
-# Hide the full path of the CC and CXX compilers since they get hardcoded here:
-#  - ./cmd/go/internal/cfg/zdefaultcc.go
-#  - ./cmd/cgo/zdefaultcc.go
-# This bug does not show up while running the tests because conda-build does
-# not remove the _build_env.
-export CC=$(basename ${CC})
-export CXX=$(basename ${CXX})
-
-
 # This is a fix for user.Current issue
 export USER="${USER:-conda}"
 export HOME="${HOME:-$(cd $SRC_DIR/..;pwd)}"
