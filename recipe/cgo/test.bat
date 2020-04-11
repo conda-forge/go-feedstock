3b5f780846115a671532628bb1bd4dc0f388c843
Patrick Sodré <psodre@gmail.com>
2020-04-11 04:07:41 -0400

    Overkill setting of TMPDIR during test

--- recipe/cgo/test.bat
+++ recipe/cgo/test.bat
@@ -3,8 +3,10 @@
 rem Put GOTMPDIR on the same drive as the conda prefix (the D drive),
 rem to avoid a known issue in the go test suite:
 rem https://github.com/golang/go/issues/24846#issuecomment-381380628
+set TMP=%PREFIX%\tmp
+set TMPDIR=%PREFIX%\tmp
 set GOTMPDIR=%PREFIX%\tmp
-mkdir "%GOTMPDIR%
+mkdir "%TMP%
 
 
 rem Batch equivalent to backticks
@@ -17,13 +19,15 @@ where go
 go env
 
 
+rem Run the failing tests by themselves, this is mostly to get logs
+rem They fail on Azure, but succeed on AppVeyor and on local Windows VM
+go tool dist test -k -v -no-rebuild -run=^^go_test:path/filepath$
+go tool dist test -k -v -no-rebuild -run=^^go_test:cmd/go$
+
+
 rem Run go's built-in test, we skip the filepath one, and cmd/go
 go tool dist test -v -no-rebuild -run=!^^go_test:path/filepath^|go_test:cmd/go$
 if errorlevel 1 exit 1
 
-rem Run the failing tests by themselves, this is mostly to get logs
-rem They fail on Azure, but succeed on AppVeyor and on local Windows VM
-go tool dist test -k -v -no-rebuild -run=^^go_test:cmd/go$
-go tool dist test -k -v -no-rebuild -run=^^go_test:path/filepath$
 
 exit 0
--- recipe/nocgo/test.bat
+++ recipe/nocgo/test.bat
@@ -3,21 +3,26 @@
 rem Put GOTMPDIR on the same drive as the conda prefix (the D drive),
 rem to avoid a known issue in the go test suite:
 rem https://github.com/golang/go/issues/24846#issuecomment-381380628
+set TMP=%PREFIX%\tmp
+set TMPDIR=%PREFIX%\tmp
 set GOTMPDIR=%PREFIX%\tmp
-mkdir "%GOTMPDIR%
+mkdir "%TMP%
 
 
 rem Diagnostics
 where go
 go env
 
-rem Run go's built-in test, we skip the filepath one, and cmd/go
-go tool dist test -k -v -no-rebuild -run=!^^go_test:path/filepath^|go_test:cmd/go$
-if errorlevel 1 exit 1
 
 rem Run the failing tests by themselves, this is mostly to get logs
 rem They fail on Azure, but succeed on AppVeyor and on local Windows VM
 go tool dist test -k -v -no-rebuild -run=^^go_test:cmd/go$
 go tool dist test -k -v -no-rebuild -run=^^go_test:path/filepath$
+
+
+rem Run go's built-in test, we skip the filepath one, and cmd/go
+go tool dist test -k -v -no-rebuild -run=!^^go_test:path/filepath^|go_test:cmd/go$
+if errorlevel 1 exit 1
+
 
 exit 0
