71c0c538acaf2b752a1669bb094a7d73fa5f106d
Patrick Sodré <psodre@gmail.com>
2020-04-11 04:44:32 -0400

    Windows go_test:path/filepath issue is solved by setting TMPDIR

--- recipe/nocgo/test.bat
+++ recipe/nocgo/test.bat
@@ -17,11 +17,10 @@ go env
 rem Run the failing tests by themselves, this is mostly to get logs
 rem They fail on Azure, but succeed on AppVeyor and on local Windows VM
 go tool dist test -k -v -no-rebuild -run=^^go_test:cmd/go$
-go tool dist test -k -v -no-rebuild -run=^^go_test:path/filepath$
 
 
 rem Run go's built-in test, we skip the filepath one, and cmd/go
-go tool dist test -k -v -no-rebuild -run=!^^go_test:path/filepath^|go_test:cmd/go$
+go tool dist test -k -v -no-rebuild -run=!^^go_test:cmd/go$
 if errorlevel 1 exit 1
 
 
