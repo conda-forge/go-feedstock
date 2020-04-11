b717ead12bffc5ada0304e60e779c9eaae4812f8
Patrick Sodré <psodre@gmail.com>
2020-04-11 02:18:58 -0400

    Using conda-forge::perl fixes net/http/cgi test on windows

--- recipe/nocgo/test.bat
+++ recipe/nocgo/test.bat
@@ -5,13 +5,12 @@ where go
 go env
 
 rem Run go's built-in test, we skip the filepath one, and cmd/go
-go tool dist test -k -v -no-rebuild -run=!^^go_test:path/filepath^|go_test:cmd/go^|go_test:net/http/cgi$
+go tool dist test -k -v -no-rebuild -run=!^^go_test:path/filepath^|go_test:cmd/go$
 if errorlevel 1 exit 1
 
 rem Run the failing tests by themselves, this is mostly to get logs
 rem They fail on Azure, but succeed on AppVeyor and on local Windows VM
 go tool dist test -k -v -no-rebuild -run=^^go_test:cmd/go$
-go tool dist test -k -v -no-rebuild -run=^^go_test:net/http/cgi$
 go tool dist test -k -v -no-rebuild -run=^^go_test:path/filepath$
 
 exit 0
