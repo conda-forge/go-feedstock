adc3f7cc5f6fa5d5810e3c72d564a8d69ad7a0a4
Patrick Sodré <psodre@gmail.com>
2020-04-11 12:56:22 -0400

    ... snafu with TMPDIRS on Windows

--- recipe/cgo/test.bat
+++ recipe/cgo/test.bat
@@ -3,8 +3,9 @@
 rem Put TMPDIR on the same drive as the conda prefix (the D drive),
 rem to avoid a known issue in the go test suite:
 rem https://github.com/golang/go/issues/24846#issuecomment-381380628
-set TMPDIR=%PREFIX%\tmp
-mkdir "%TMPDIR%
+set TMP=%PREFIX%\tmp
+set TMPDIR=%TMP%
+mkdir "%TMP%
 
 
 rem Batch equivalent to backticks
