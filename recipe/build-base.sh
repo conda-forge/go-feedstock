14ce03ba4b08ffd2011a68cbeb3e59d472ec59fb
Patrick Sodré <psodre@gmail.com>
2020-04-12 01:04:11 -0400

    Use precompiled compiler for bootstrapping

--- recipe/build-base.sh
+++ recipe/build-base.sh
@@ -5,6 +5,17 @@ export USER="${USER:-conda}"
 export HOME="${HOME:-$(cd $SRC_DIR/..;pwd)}"
 
 
+#
+# Use precompiled bootstrap
+case $ARCH in
+  aarch64|ppc64le)
+    export GOROOT_BOOTSTRAP=$SRC_DIR/go-bootstrap
+    ;;
+  *)
+    ;;
+esac
+
+
 # Do not use GOROOT_FINAL. Otherwise, every conda environment would
 # need its own non-hardlinked copy of the go (+100MB per env).
 # It is better to rely on setting GOROOT during environment activation.
