4d824a521ec04b38b8b1737464e32b496b0ba7a2
Patrick Sodré <psodre@gmail.com>
2020-04-23 19:14:45 -0400

    Enable runtime/race check on windows. Run skipped test that require a GO_BUILDER_NAME

--- recipe/cgo/test.bat
+++ recipe/cgo/test.bat
@@ -20,6 +20,12 @@ go env
 rem Expect PASS
 go tool dist test -v -no-rebuild -run=!^^go_test:cmd/go$
 if errorlevel 1 exit 1
+go tool dist test -v -no-rebuild -race -run='^go_test:runtime/race$'
+if errorlevel 1 exit 1
+set GO_BUILDER_NAME=windows-amd64-condaforge
+go tool dist test -run='^testcshared|testcarchive$'
+if errorlevel 1 exit 1
+set "GO_BUILDER_NAME="
 
 
 exit 0
--- recipe/nocgo/test.bat
+++ recipe/nocgo/test.bat
@@ -15,6 +15,12 @@ go env
 rem Expect PASS
 go tool dist test -v -no-rebuild
 if errorlevel 1 exit 1
+go tool dist test -v -no-rebuild -race -run='^go_test:runtime/race$'
+if errorlevel 1 exit 1
+set GO_BUILDER_NAME=windows-amd64-condaforge
+go tool dist test -run='^testcshared|testcarchive$'
+if errorlevel 1 exit 1
+set "GO_BUILDER_NAME="
 
 
 exit 0
