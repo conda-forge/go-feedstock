bc034fb51fabae6b0f49a274ef8686289c271d0d
Patrick Sodré <psodre@gmail.com>
2020-04-24 10:24:04 -0400

    Enable all tests for windows cgo

--- recipe/cgo/test.bat
+++ recipe/cgo/test.bat
@@ -18,10 +18,12 @@ go env
 
 
 rem Expect PASS
-go tool dist test -v -no-rebuild -run=!^^go_test:cmd/go$
+go tool dist test -v -no-rebuild
 if errorlevel 1 exit 1
 go tool dist test -v -no-rebuild -race -run='^go_test:runtime/race$'
 if errorlevel 1 exit 1
+
+rem Impersonate go builder
 set GO_BUILDER_NAME=windows-amd64-condaforge
 go tool dist test -run='^testcshared^|testcarchive$'
 if errorlevel 1 exit 1
--- recipe/nocgo/test.bat
+++ recipe/nocgo/test.bat
@@ -17,7 +17,6 @@ go tool dist test -v -no-rebuild
 if errorlevel 1 exit 1
 go tool dist test -v -no-rebuild -race -run='^go_test:runtime/race$'
 if errorlevel 1 exit 1
-set "GO_BUILDER_NAME="
 
 
 exit 0
