6cf88c90facb72045b1387c1b7c09386f7bcba3a
Marius van Niekerk <marius.v.niekerk@gmail.com>
2022-01-10 12:24:41 +0200

    Add bootstrap sources for windows

--- recipe/build-base.bat
+++ recipe/build-base.bat
@@ -4,7 +4,6 @@ rem It is better to rely on setting GOROOT during environment activation.
 rem
 rem c.f. https://github.com/conda-forge/go-feedstock/pull/21#discussion_r202513916
 set "GOROOT=%SRC_DIR%\go"
-set "GOCACHE=off"
 
 
 rem Print diagnostics before starting the build
