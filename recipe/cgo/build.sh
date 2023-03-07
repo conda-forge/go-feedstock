3499fac508a51a35b194a16bee000cf245797bcf
Marius van Niekerk <marius.v.niekerk@gmail.com>
2023-03-07 13:36:50 -0500

    Try to get cgo linux build working

--- recipe/cgo/build.sh
+++ recipe/cgo/build.sh
@@ -13,7 +13,7 @@ case $(uname -s) in
     ;;
   Linux)
     # We have to disable garbage collection for sections
-    export CGO_LDFLAGS="${CGO_LDFLAGS} -Wl,--no-gc-sections"
+    export CGO_LDFLAGS="${CGO_LDFLAGS} -lrt -Wl,--no-gc-sections"
     ;;
   *)
     echo "Unknown OS: $(uname -s)"
