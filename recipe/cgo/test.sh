04ae4c056fc98f67a2fe19e5bf0bff6a0e0b72a4
Uwe L. Korn <uwe.korn@quantco.com>
2025-07-10 17:31:46 +0200

    Drop duplicate rpath in tests

--- recipe/cgo/test.sh
+++ recipe/cgo/test.sh
@@ -18,6 +18,11 @@ test "$(go env CGO_ENABLED)" == 1
 export CC=$(basename $CC)
 go build -x runtime/cgo
 
+if [[ "$(go env GOHOSTOS)" == "darwin" ]]; then
+  # Drop this as it is anyways part of the flags and the tests are sensitive to linker warnings
+  export LDFLAGS="${LDFLAGS/-Wl,-rpath,$CONDA_PREFIX\/lib/ }"
+fi
+
 # Run go's built-in test
 case $(uname -s) in
   Darwin)
